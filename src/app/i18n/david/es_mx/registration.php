7863249855ac44092a1ce1c54890d45513fca465
David Velasquez <david.v@wagento.com>
2016-04-05 09:38:13 -0400

    added comment to files

--- src/app/i18n/david/es_mx/registration.php
+++ src/app/i18n/david/es_mx/registration.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Build By: Jose David Velasquez Ricaldez
+ * Email: JDavidVR@hotmail.com  davidvr1710@gmail.com david.v@wagento.com
+ * Cbba - Bolivia
+ */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
